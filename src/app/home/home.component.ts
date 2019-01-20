4efd7710f07561b72e791c49d08d89a67a659740
Paul Arduin <paul.arduin.pro@gmail.com>
2019-01-20 11:22:16 +0100

    Ajout de la notion hot pour la hotBar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  
   products= [
-    {name: 'Gérard Depardieu',    descriptionShort: 'Votre parfait compagnon de beuverie',                image:'assets/AngularStarsPictures/Depardieu.jpg',  price:'350€'},
-    {name: 'Liliane Bettencourt', descriptionShort: 'Gagne en 18 minutes ce que vous gagnez en une vie',  image:'assets/AngularStarsPictures/Liliane.jpg',    price:'15€'},
-    {name: 'Cyril Hanouna',       descriptionShort:'Ce bon vieu Baba saura vous faire rire',              image:'assets/AngularStarsPictures/Hanuna.jpeg',    price:'180€'},
+    {id: 1, name: 'Gérard Depardieu',    descriptionShort: 'Votre parfait compagnon de beuverie',                image:'assets/AngularStarsPictures/Depardieu.jpg',  price:'350€',  hot:true},
+    {id: 2, name: 'Liliane Bettencourt', descriptionShort: 'Gagne en 18 minutes ce que vous gagnez en une vie',  image:'assets/AngularStarsPictures/Liliane.jpg',    price:'15€',   hot:true},
+    {id: 3, name: 'Cyril Hanouna',       descriptionShort: 'Ce bon vieu Baba saura vous faire rire',             image:'assets/AngularStarsPictures/Hanuna.jpeg',    price:'180€',  hot:false},
+    {id: 4, name: 'Michèle Mathy',       descriptionShort: 'Vous sauve la vie en un claquement de doigts',       image:'assets/AngularStarsPictures/MimiMathy.jpg',  price:'1500€', hot:true},
+    {id: 5, name: 'Jean-Pierre Coffe',   descriptionShort: 'La crème de la critique gastronomique Française',    image:'assets/AngularStarsPictures/Coffe.jpeg',      price:'480€',  hot:true}
   ]
   constructor() { }
 
